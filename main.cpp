19250ec422258ac60cf2cf226c03c40f37b77d3e
UponTheSky <89925675+UponTheSky@users.noreply.github.com>
2023-09-22 16:15:46 +0900

    basic cmake setup (#2)
    * basic cmake setup
    
    * cmake: setup glfw and glad
    
    * add macos frameworks

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+
+  std::cout << "My test drive, just like the one of Hiccup and Toothless" << std::endl;
+
+  return 0;
+}
